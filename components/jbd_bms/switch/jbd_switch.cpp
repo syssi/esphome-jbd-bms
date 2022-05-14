53bf7a8d1b7f8a528008c7ebcaec3c92591f6757
Sebastian Muszynski <basti@linkt.de>
2022-05-14 14:27:35 +0200

    Disable write_state because it's untested and incomplete

--- components/jbd_bms/switch/jbd_switch.cpp
+++ components/jbd_bms/switch/jbd_switch.cpp
@@ -8,7 +8,9 @@ namespace jbd_bms {
 static const char *const TAG = "jbd_bms.switch";
 
 void JbdSwitch::dump_config() { LOG_SWITCH("", "JbdBms Switch", this); }
-void JbdSwitch::write_state(bool state) { this->parent_->write_register(this->holding_register_, (uint16_t) state); }
+void JbdSwitch::write_state(bool state) {
+  // this->parent_->write_register(this->holding_register_, (uint16_t) state);
+}
 
 }  // namespace jbd_bms
 }  // namespace esphome
