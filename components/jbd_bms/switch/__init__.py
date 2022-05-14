952a4bead1e08fe5d1c40510dfc8e1dd00502681
Sebastian Muszynski <basti@linkt.de>
2022-05-14 18:30:46 +0200

    Use async await

--- components/jbd_bms/switch/__init__.py
+++ components/jbd_bms/switch/__init__.py
@@ -54,14 +54,14 @@ CONFIG_SCHEMA = cv.Schema(
 )
 
 
-def to_code(config):
-    hub = yield cg.get_variable(config[CONF_JBD_BMS_ID])
+async def to_code(config):
+    hub = await cg.get_variable(config[CONF_JBD_BMS_ID])
     for key, address in SWITCHES.items():
         if key in config:
             conf = config[key]
             var = cg.new_Pvariable(conf[CONF_ID])
-            yield cg.register_component(var, conf)
-            yield switch.register_switch(var, conf)
+            await cg.register_component(var, conf)
+            await switch.register_switch(var, conf)
             cg.add(getattr(hub, f"set_{key}_switch")(var))
             cg.add(var.set_parent(hub))
             cg.add(var.set_holding_register(address))
