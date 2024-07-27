0edacc3d79a6e848c1a5dc1fadd6ec64f7681ed0
Sebastian Muszynski <basti@linkt.de>
2024-07-27 10:16:38 +0200

    Move fake traffic from code to YAML (Closes: #82) (#85)

--- components/jbd_bms_ble/__init__.py
+++ components/jbd_bms_ble/__init__.py
@@ -9,7 +9,6 @@ AUTO_LOAD = ["binary_sensor", "sensor", "switch", "text_sensor"]
 MULTI_CONF = True
 
 CONF_JBD_BMS_BLE_ID = "jbd_bms_ble_id"
-CONF_ENABLE_FAKE_TRAFFIC = "enable_fake_traffic"
 
 jbd_bms_ble_ns = cg.esphome_ns.namespace("jbd_bms_ble")
 JbdBmsBle = jbd_bms_ble_ns.class_(
@@ -20,7 +19,6 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(JbdBmsBle),
-            cv.Optional(CONF_ENABLE_FAKE_TRAFFIC, default=False): cv.boolean,
         }
     )
     .extend(ble_client.BLE_CLIENT_SCHEMA)
@@ -32,5 +30,3 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await ble_client.register_ble_node(var, config)
-
-    cg.add(var.set_enable_fake_traffic(config[CONF_ENABLE_FAKE_TRAFFIC]))
