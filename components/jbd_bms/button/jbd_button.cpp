08ccc31d0f191a552f8335ac6bebe06af1da4f9b
Sebastian Muszynski <basti@linkt.de>
2024-07-28 20:42:58 +0200

    Fix

--- components/jbd_bms/button/jbd_button.cpp
+++ components/jbd_bms/button/jbd_button.cpp
@@ -7,6 +7,8 @@ namespace jbd_bms {
 
 static const char *const TAG = "jbd_bms.button";
 
+static const uint8_t JBD_CMD_READ = 0xA5;
+
 void JbdButton::dump_config() { LOG_BUTTON("", "JbdBms Button", this); }
 void JbdButton::press_action() { this->parent_->send_command(JBD_CMD_READ, this->holding_register_); }
 
